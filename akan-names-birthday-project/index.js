1397f42dcf5a32858710b23f8de197db34035214
John Powell <powell.john2011@gmail.com>
2022-03-07 14:33:09 +0300

    initial files

---
+++ akan-names-birthday-project/index.js
@@ -0,0 +1,46 @@
+const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi", "Kwame"];
+const daysOfTheWeek =["Sunday","Monday" ,"Tuesday","Wednesday ","Thursday ","Friday","Saturday" ];
+const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+let century = "";
+let year = "";
+let month = "";
+let day = "";
+let dayOfTheWeek= "";
+let akanName = ""
+
+const getGenderName = () => {
+    const fGender = document.getElementById("female").value;
+    const mGender = document.getElementById("male").value;
+    if(fGender) {
+        return getAkanName(femaleNames)
+    } else if( mGender) {
+        return getAkanName(maleNames)
+    }
+}
+
+const getAkanName = (genderArray) => {
+    for(let i = 0; i<genderArray.length; i++){
+        if(daysOfTheWeek[i]=== dayOfTheWeek){
+            akanName = genderArray[i];
+        }
+    }
+}
+
+const calculateDayOfTheWeek = (century, year, month , day)=> {
+    return ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7;
+}
+document.getElementById("show-akan-name").style.display = 'none';
+
+const getUserInput = function (event) {
+    event.preventDefault();
+    let dateOfBirth = document.getElementById("birth-date").value;
+    let dateDetails = dateOfBirth.split('-')
+    century = parseInt(dateDetails[0].slice(0,2));
+    year = parseInt(dateDetails[0].slice(2,4));
+    month = parseInt(dateDetails[1]);
+    day = parseInt(dateDetails[2]);
+    dayOfTheWeek = calculateDayOfTheWeek(century,year,month,day)
+    console.log(month);
+    
+}
